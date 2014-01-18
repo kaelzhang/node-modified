90e644d84fb9c94ffc03ba528b053e89b09f8ed3
kaelzhang <i@kael.me>
2014-01-18 20:48:30 +0800

    #1: modified instances will now be streams; untested; progressly commit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var Stream      = require('stream').Stream;
 var util        = require('util');
 var node_path   = require('path');
 
+var async       = require('async');
+
+var tool        = require('./lib/tool');
+
 
 // @param {Object} options
 function Modified (options) {
@@ -15,14 +19,21 @@ function Modified (options) {
     options = options || {};
 
     // you can also inherit and override 'read' and 'save' methods
-    ['read', 'save', 'cacheMapper'].forEach(function (key) {
+    [
+        // 'read', 
+        // 'save', 
+        'cacheMapper'
+    ].forEach(function (key) {
         if ( options[key] ) {
             this['_' + key] = options[key];
         }
 
     }, this);
 
-    this.options = options;
+    this.options = this._defaults(options, {
+        // strict: false
+    });
+
     this._pipeQueue = [];
 }
 
@@ -38,47 +49,61 @@ var IF_NONE_MATCH = 'if-none-match';
 var IF_MODIFIED_SINCE = 'if-modified-since';
 
 
+Modified.prototype._defaults = function(options, defaults) {
+    var key;
+    for (key in defaults) {
+        if ( !(key in options) ) {
+            options[key] = defaults[key];
+        }
+    }
+
+    return options;
+};
+
+
 Modified.prototype.request = function(options, callback) {
     var self = this;
 
-    this._read(options, function (read_err, headers, data) {
-        if ( !options.headers ) {
-            options.headers = {};
-        }
+    this._readCacheInfo(options, function (err, info) {
+        if ( !err ) {
+            var headers = info.headers;
 
-        if ( headers ) {
-            // deal with etag
-            if ( ETAG in headers ) {
-                options.headers[IF_NONE_MATCH] = headers[ETAG];
-            }
-
-            if ( LAST_MODIFIED in headers ) {
-                options.headers[IF_MODIFIED_SINCE] = headers[LAST_MODIFIED];
-            }
-
-        } else {
-            delete options.headers[IF_NONE_MATCH];
-            delete options.headers[IF_MODIFIED_SINCE];
-        }
-
-        if ( callback ) {
-            request(options, function (err, res, body) {
-                if ( err ) {
-                    return callback(err, res, body);
+            if ( headers ) {
+                // deal with etag
+                if ( ETAG in headers ) {
+                    options.headers[IF_NONE_MATCH] = headers[ETAG];
                 }
 
-                if ( res.statusCode === 304 ) {
-                    return callback(err, res, data || body);
+                if ( LAST_MODIFIED in headers ) {
+                    options.headers[IF_MODIFIED_SINCE] = headers[LAST_MODIFIED];
                 }
 
-                self._save(options, res.headers, body, function (save_err) {
-                    callback(err, res, body);
-                });
-            });
-        
-        } else {
-            request(options);
+            } else {
+                delete options.headers[IF_NONE_MATCH];
+                delete options.headers[IF_MODIFIED_SINCE];
+            }
         }
+
+        var request = request(options, function (err, res, body) {
+            if ( err ) {
+                return callback(err, res, body);
+            }
+
+            if ( res.statusCode === 304 ) {
+                return self._sendResponseBody(res, info.data_file, callback);
+            }
+
+            self._saveResponse(res, body, info);
+        });
+
+        request.on('response', function (response) {
+            self._response = response;
+            self.emit('response', response); 
+        });
+
+        request.on('data', function (data) {
+            self.emit('data', data);
+        });
     });
 
     // So, modified.request is a stream
@@ -86,40 +111,37 @@ Modified.prototype.request = function(options, callback) {
 };
 
 
-// If-None-Match
-// If-Modified-Since
-
-// read local cache
-Modified.prototype._read = function(options, callback) {
-    var self = this;
-
-    this._mapCache(options, function (err, file) {
-        if ( err ) {
-            return callback(err);
-        }
-
-        if ( !file ) {
-            return callback(null);
-        }
-
-        var content;
-
-        try {
-            content = fs.read(file);
-        } catch(e) {
-            return callback(e);
-        }
-
-        var info = modified.parse(content.toString());
-        callback(null, info.headers, info.data);
+Modified.prototype._sendResponseBody = function(res, file, callback) {
+    fs.readFile(file, function (err, content) {
+        callback(null, res, content);
     });
 };
 
 
-// save data to local cache
-// @param {Object} options
-// - data: {Object} data to be saved
-Modified.prototype._save = function(options, headers, data, callback) {
+// Save responses into cache files
+Modified.prototype._saveResponse = function(res, body, info, callback) {
+    async.parallel([
+        function (done) {
+            fs.writeFile(info.data_file, body, done)
+        },
+
+        function (done) {
+            fs.writeFile(info.header_file, JSON.stringify(res.headers), done)
+        }
+
+    ], function (err) {
+        callback(err, res, body);
+    })
+};
+
+
+// If-None-Match
+// If-Modified-Since
+
+// Read local cache information
+// TODO:
+// 
+Modified.prototype._readCacheInfo = function(options, callback) {
     var self = this;
 
     this._mapCache(options, function (err, file) {
@@ -131,25 +153,62 @@ Modified.prototype._save = function(options, headers, data, callback) {
             return callback(null);
         }
 
-        var cache = modified.stringify(headers, data);
+        var data_file = self._getDataPath(file);
+        var file_content;
 
-        try {
-            fs.write(file, cache);
-            callback(null);
-        } catch(e) {
-            callback(e);
-        }
+        async.parallel([
+            function (done) {
+                fs.readFile(file, function (err, content) {
+                    file_content = content;
+                    done(err);
+                });
+            },
+
+            function (done) {
+                fs.exists(data_file, function (exists) {
+                    done(!exists);
+                });
+            }
+
+        ], function (err) {
+            if ( err ) {
+                return callack(err);
+            }
+
+            var headers;
+
+            try {
+                headers = JSON.parse(file_content);
+
+            } catch(e) {
+                return callback({
+                    code: 'EPARSEPKG',
+                    message: 'error parsing JSON: ' + e.stack,
+                    data: {
+                        error: e
+                    }
+                });
+            }
+
+            callback(null, {
+                headers: headers,
+                data_file: data_file,
+                header_file: file
+            });
+        });
     });
 };
 
 
 // Get the path of the cached binary file
-Modified.prototype._getFilePath = function(filepath, url) {
-    var dir = node_path.dirname(filepath);
-    var basename = node_path.basename(url);
-
-    return node_path.join(dir, basename);
+Modified.prototype._getDataPath = function(filepath) {
+    return filepath + '.modified-data';
 };
+
+
+// Modified.prototype._tempDataPath = function(filepath) {
+//     return 
+// };
 
 
 Modified.prototype.pipe = function(dest, options) {
@@ -169,6 +228,7 @@ Modified.prototype.pipe = function(dest, options) {
 };
 
 
+// The real pipe
 Modified.prototype._pipe = function(dest, options) {
     Stream.prototype.pipe.call(this, dest, options);
 };
@@ -196,6 +256,11 @@ Modified.prototype._mapCache = function(options, callback) {
         // ```
         callback(null);
     }
+};
+
+
+Modified.prototype._pipeBinary = function(file, dest, options) {
+    fs.createReadStream(file).pipe(dest, options);
 };
 
 
