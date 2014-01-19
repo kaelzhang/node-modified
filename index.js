eb1b40fcf77fcd618360cc5a6fb38bd10963fa13
kaelzhang <i@kael.me>
2014-01-19 23:29:15 +0800

    #10: main: completely refractor the main entrance; modified(options) will return a ghost object that has nearly the same usage as request

--- index.js
+++ index.js
@@ -3,29 +3,132 @@
 module.exports = modified;
 modified.Modified = Modified;
 
-
 var Modified = require('./modified');
+var request = require('request');
 
-function modified (options) {
-    return new Modified(options);
+function modified(options) {
+    function req(uri, options, callback) {
+        return request.get(uri, options, callback);
+    };
+
+    req._options = options;
+    req.__proto__ = Ghost.prototype;
+
+    return req;
 }
 
 
-modified.get = function () {
-    
+// A ghost prototype to generate the real exports
+function Ghost(options) {}
+
+Ghost.prototype._defaults = function(options, defaults) {
+    var key;
+    for (key in defaults) {
+        if (!(key in options)) {
+            options[key] = defaults[key];
+        }
+    }
+
+    return options;
 };
 
 
-modified.put = function () {
-    
+Ghost.prototype._clone = function(object) {
+    var key;
+    var cloned = {};
+    for (key in object) {
+        cloned[key] = object[key];
+    }
+
+    return cloned;
 };
 
 
-modified.post = function () {
-    
+Ghost.prototype.initParams = request.initParams;
+
+
+// Merge the options of modified
+Ghost.prototype._initParams = function(uri, options, callback) {
+    var opts = this.initParams(uri, options, callback);
+    var opts_options = opts.options;
+    var self = this;
+
+    Modified.OPTIONS_LIST.forEach(function(key) {
+        if (!(key in opts_options)) {
+            opts_options[key] = self._options[key];
+        }
+    });
+
+    return opts;
 };
 
 
-modified.del = function () {
-    
+Ghost.prototype._request = function(uri, options, callback) {
+    if (typeof uri === 'undefined') {
+        throw new Error('undefined is not a valid uri or options object.');
+    }
+
+    if ((typeof options === 'function') && !callback) {
+        callback = options;
+    }
+
+    if (options && typeof options === 'object') {
+        options.uri = uri;
+
+    } else if (typeof uri === 'string') {
+        options = {
+            uri: uri
+        };
+
+    } else {
+        options = uri;
+    }
+
+    options = this._clone(options);
+
+    if (callback) {
+        options.callback = callback;
+    }
+
+    var m = new Modified(options);
+    return m;
 };
+
+
+Ghost.make = function(method, mutator) {
+    return function(uri, options, callback) {
+        var params = this._initParams(uri, options, callback);
+        params.options.method = method;
+
+        if (mutator) {
+            mutator(options);
+        }
+
+        return this._request(param.uri || null, params.options, params.callback);
+    };
+};
+
+
+[
+    'get',
+    'post',
+    'patch',
+    'del',
+    'put'
+
+].forEach(function (method) {
+    Ghost.prototype[method] = Ghost.make(method.toUpperCase());
+});
+
+
+Ghost.prototype.head = Ghost.make('HEAD', function(options) {
+    if (
+        params.options.body ||
+        params.options.requestBodyStream ||
+        params.options.json && typeof params.options.json !== 'boolean' ||
+        params.options.multipart
+    ) {
+        throw new Error("HTTP HEAD requests MUST NOT include a request body.")
+    }
+});
+
